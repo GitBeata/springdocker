aad9916cfcd0b9a0dc4f76c27ce171035818de01
--global <--global>
2025-01-24 19:09:11 +0100

    controller

--- projekt-spring-boot-2/src/main/java/com/aplikacja/przyklad1/controller/UserController.java
+++ projekt-spring-boot-2/src/main/java/com/aplikacja/przyklad1/controller/UserController.java
@@ -4,12 +4,14 @@ package com.aplikacja.przyklad1.controller;
 import com.aplikacja.przyklad1.model.User;
 import com.aplikacja.przyklad1.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.ui.Model;
 
+
 import java.util.List;
 
-@RestController
+@Controller
 @RequestMapping("/users")
 public class UserController {
     @Autowired
